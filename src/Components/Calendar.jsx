176e6e5ec08c976b24781f9ee8da2449529cfffd
Thomas Laird <t6r6l5@gmail.com>
2017-11-10 17:45:29 -0700

    map directly over result of calendar generation func - fixes async issue

--- src/Components/Calendar.jsx
+++ src/Components/Calendar.jsx
@@ -28,7 +28,7 @@ let months = [
     lastDay: 31
   },
   {
-    month: "November",
+    month: "April",
     firstDay: 1,
     lastDay: 30
   }  
@@ -49,7 +49,6 @@ class Calendar extends Component {
   }
 
   forwardMonth () {
-    this.generateMonthStructure();
     this.setState( {
       month: months[Math.min(5, this.state.monthIndex + 1)].month,
       monthIndex: Math.min(5, this.state.monthIndex + 1)
@@ -57,21 +56,20 @@ class Calendar extends Component {
   }
 
   backwardMonth () {
-    this.generateMonthStructure();
     this.setState( {
       month: months[Math.max(0, this.state.monthIndex - 1)].month,
       monthIndex: Math.max(0, this.state.monthIndex - 1)
     });
   }
 
-  generateMonthStructure () {
+  generateMonthStructure (monthIndex) {
     let dayCount = 0;
-    let totalCount = months[this.state.monthIndex].firstDay + months[this.state.monthIndex].lastDay;
+    let totalCount = months[monthIndex].firstDay + months[monthIndex].lastDay;
     let result = [];
 
     for (dayCount ; dayCount <= 41; dayCount ++ ) {
-      if (!(dayCount < months[this.state.monthIndex].firstDay - 1) && dayCount < totalCount - 1) {
-        result.push(dayCount - months[this.state.monthIndex].firstDay + 2);
+      if (!(dayCount < months[monthIndex].firstDay - 1) && dayCount < totalCount - 1) {
+        result.push(dayCount - months[monthIndex].firstDay + 2);
       } else {
         result.push('');
       }
@@ -81,21 +79,20 @@ class Calendar extends Component {
       result = result.slice(0, 35);
     }
 
-    this.setState({
-      monthStructure: result
-    });
-    
-    console.log('generate map array');
-    console.log(result);
+    // this.setState({
+    //   monthStructure: result
+    // });
+
+    return result;
   }
 
   // componentDidMount () {
   //   this.generateMonthStructure();
   // }
 
-  componentWillMount () {
-    this.generateMonthStructure();
-  }
+  // componentWillMount () {
+  //   this.generateMonthStructure();
+  // }
 
 
   render () {
@@ -106,7 +103,7 @@ class Calendar extends Component {
         <div className="Calendar">
           <h3>{this.state.month}</h3>
           <div className="week">  
-            {this.state.monthStructure.map(day => {
+            {this.generateMonthStructure(this.state.monthIndex).map(day => {
               return (<div className="day">{day}</div>);
             })}
           </div>          
